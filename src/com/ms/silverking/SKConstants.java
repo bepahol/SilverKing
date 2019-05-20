9754f4dd0c32d5a9e10837f4e73ec3277b649654
Benjamin Holst <bepahol2@gmail.com>
2019-05-20 16:49:37 -0600

    Saving files before refreshing line endings

--- src/com/ms/silverking/SKConstants.java
+++ src/com/ms/silverking/SKConstants.java
@@ -2,6 +2,5 @@ package com.ms.silverking;
 
 public class SKConstants {
 	public static final String javaHomeEnv = "SK_JAVA_HOME";
-    public static final String test = "test";
-    public static final String test2 = "another test";
+	public static final String test = "test2";
 }
