44a1ad4066d1f3d904d4045cec2e0306d5fc109a
Benjamin Holst <bepahol2@gmail.com>
2019-05-20 16:48:35 -0600

    test

--- src/com/ms/silverking/SKConstants.java
+++ src/com/ms/silverking/SKConstants.java
@@ -3,4 +3,5 @@ package com.ms.silverking;
 public class SKConstants {
 	public static final String javaHomeEnv = "SK_JAVA_HOME";
     public static final String test = "test";
+    public static final String test2 = "another test";
 }
